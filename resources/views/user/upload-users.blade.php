b93364b10b393508568050eb581b105b0307b4cc
sundwizard <emberugbon@gmail.com>
2024-10-08 14:31:23 +0100

    bulk user upload

--- resources/views/user/upload-users.blade.php
+++ resources/views/user/upload-users.blade.php
@@ -1,18 +1,25 @@
-<div class="modal" id="uploadUsers" tabindex="-1" role="dialog" wire:ignore>
+<div class="modal" id="uploadUsers" tabindex="-1" role="dialog" wire:ignore.self>
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-body">
-                {{Form::open(array('url'=>'upload-users','enctype'=>'multipart/form-data','method'=>'post'))}}
+                <form wire:submit.prevent="uploadUser" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="modal-header">
-                    <h5 class="modal-title" id="applyLeave">Upload Users
-                    </h5>
+                            <h5 class="modal-title" id="applyLeave">Upload Users </h5>
                 </div>
                     <div class="modal-body">
                         <div class="row">
-                            <div class="col-md-6 mt-1">
+                            @if($failed_upload)
+                            <div class="alert alert-danger alert-dismissible alert-alt fade show mt-3">
+                                <strong>Attention! </strong> Some records were not uploaded. kindly ensure you <br> entered all records correctly.
+                                <input type="button"  wire:click.prevent="downloadFailedUpload" value="{{__('Donlaod Failed Upload')}}" class="btn  btn-danger">
+                            </div>
+                        @endif
+                            <div class="col-md-8 mt-1">
                                 <div class="form-group">
-                                    {{Form::file('uploadFile',null,array('class'=>'form-control','placeholder'=>__('Select File'),'required'=>'required'))}}
+                                    <input type="file" wire:model="uploadFile" class="form-control">
+                                    <div style="color: red" align="center" wire:loading wire:target="uploadFile" >Loading...</div>
+
                                     @error('uploadFile')
                                     <small class="invalid-name" role="alert">
                                         <strong class="text-danger">{{ $message }}</strong>
@@ -20,18 +27,18 @@
                                     @enderror
                                 </div>
                             </div>
-                            <div class="col-md-6 ">
-                                <input type="submit" value="{{__('Download Template')}}" class="btn  btn-primary">
+                            <div class="col-md-4 ">
+                                <a href="{{ asset('uploads/staf-sample.xlsx') }}"><input type="button" value="{{__('Download Template')}}" class="btn  btn-primary"></a>
                             </div>
                         </div>
 
                     </div>
 
                     <div class="modal-footer">
-                        <input type="button" value="{{__('Cancel')}}" class="btn  btn-light" data-bs-dismiss="modal">
+                        <input type="button" id="closeUploadUser" value="{{__('Cancel')}}" class="btn  btn-light" data-bs-dismiss="modal">
                         <input type="submit" value="{{__('Onboard Users')}}" class="btn  btn-primary">
                     </div>
-                {{Form::close()}}
+                </form>
             </div>
         </div>
     </div>
@@ -43,6 +50,12 @@
             console.log('user');
             document.getElementById("toggleOldUser").click();
         });
+    </script>
+
+    <script>
+        window.addEventListener('success', event => {
+            document.getElementById("closeUploadUser").click();
+        })
     </script>
     @endif
 @endpush
