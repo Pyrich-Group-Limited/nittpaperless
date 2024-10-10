00cdf577e9d3681e3978d61b23535d210792d9f4
Kamzone IT <kamzoneintegratedtech2019@gmail.com>
2024-10-10 01:07:36 +0100

    fix

--- storage/framework/views/8ab889e33d48ce18ae7d2ec43b1d5672360cca27.php
+++ storage/framework/views/8ab889e33d48ce18ae7d2ec43b1d5672360cca27.php
@@ -163,7 +163,7 @@
                                             
                                             
                                             <div class="action-btn bg-primary ms-2">
-                                                <a href="#" data-size="lg" data-bs-toggle="modal" data-bs-target="#publishAdvertModal" id="toggleOldProject" wire:click="selProject(<?php echo e($project->id); ?>)"  data-bs-toggle="tooltip" title="<?php echo e(__('Advertise Project')); ?>"  class="mx-3 btn btn-sm d-inline-flex align-items-center">
+                                                <a href="#" data-size="lg" data-bs-toggle="modal" data-bs-target="#publishAdvertModal" id="toggleOldProject" wire:click="selProject2(<?php echo e($project); ?>)"  data-bs-toggle="tooltip" title="<?php echo e(__('Advertise Project')); ?>"  class="mx-3 btn btn-sm d-inline-flex align-items-center">
                                                     <i class="ti ti-share text-white"></i>
                                                 </a>
                                             </div>
